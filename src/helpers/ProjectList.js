1d25a6f811b1aebac6e9edbdd6094d3556b8b474
Dgibson89 <donovan.gibson89@gmail.com>
2023-10-04 14:57:11 -0500

    Added 3 more projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,31 +1,56 @@
-import LawnCare from "../assets/lawncare.jpg"
-import TravelJournal from "../assets/hauntedtraveljournal.jpg"
-import Massage from "../assets/massagelp.jpg"
-
-
+import LawnCare from "../assets/lawncare.jpg";
+import TravelJournal from "../assets/hauntedtraveljournal.jpg";
+import Massage from "../assets/massagelp.jpg";
+import Portfolio from "../assets/firstportfolio.jpg";
+import AnimeBrowser from "../assets/animebrowserapp.jpg";
+import Quote from "../assets/randomquote.jpg"
 export const ProjectList = [
-    {
-        name: "Lawn Care LP",
-        image: LawnCare,
-        alttxt: "",
-        skills: "HTML. CSS",
-        gitHub: "https://github.com/Dgibson89/EderLandingPage",
-        website: "https://scintillating-lebkuchen-0f10c5.netlify.app/",
-    },
-    {
-        name: "Haunted Travel Journal",
-        image: TravelJournal,
-        alttxt: "",
-        skills: "React, CSS",
-        gitHub: "https://github.com/Dgibson89/traveljournal",
-        website: "https://traveljournal-e94h1wjfm-dgibson89.vercel.app/",
-    },
-    {
-        name: "Massage LP",
-        image: Massage,
-        alttxt: "",
-        skills: "HTML, CSS",
-        gitHub: "https://github.com/Dgibson89/MonicaLandingP",
-        website: "https://peppy-semolina-d01866.netlify.app/",
-    }
-]
+  {
+    name: "Lawn Care LP",
+    image: LawnCare,
+    alttxt: "Lawn Care",
+    skills: "HTML. CSS",
+    gitHub: "https://github.com/Dgibson89/EderLandingPage",
+    website: "https://scintillating-lebkuchen-0f10c5.netlify.app/",
+  },
+  {
+    name: "Haunted Travel Journal",
+    image: TravelJournal,
+    alttxt: "Haunted Travel Journal",
+    skills: "React, CSS",
+    gitHub: "https://github.com/Dgibson89/traveljournal",
+    website: "https://traveljournal-e94h1wjfm-dgibson89.vercel.app/",
+  },
+  {
+    name: "Massage LP",
+    image: Massage,
+    alttxt: "Massage Landing Page",
+    skills: "HTML, CSS",
+    gitHub: "https://github.com/Dgibson89/MonicaLandingP",
+    website: "https://peppy-semolina-d01866.netlify.app/",
+  },
+  {
+    name: "Anime Character Browser",
+    image: AnimeBrowser,
+    alttxt: "Anime Character Browser",
+    skills: "React, Bootstrap",
+    gitHub: "https://github.com/Dgibson89/animecharacterbrowser",
+    website: "https://animecharacterbrowser.vercel.app/",
+  },
+  {
+    name: "Retired Portfolio",
+    image: Portfolio,
+    alttxt: "First Portfolio",
+    skills: "HTML, CSS, JavaScript",
+    gitHub: "https://github.com/Dgibson89/PortfolioJS",
+    website: "https://dgibson89.netlify.app/",
+  },
+  {
+    name: "Random Daily Quote",
+    image: Quote,
+    alttxt: "Inspiring Quote",
+    skills: "React, Bootstrap",
+    gitHub: "https://github.com/Dgibson89/randomquotereact",
+    website: "https://randomquotereact.vercel.app/",
+  },
+];
